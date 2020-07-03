d5c8ca57724dd24efc384646d575c012672717c0
Pengc <pengcheng612@gmail.com>
2020-07-03 11:26:25 -0700

    tools

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, NavLink } from 'react-router-dom';
+import { Menu } from 'antd';
+import Router from './router';
+
+// import 'antd/dist/antd.css';   less:+ require: override, lessLoader
+// import 'antd/dist/antd.less';  按需加载: require: override, babel
+
+import styles from "./App.module.less" // avoid name conflict
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Menu className={styles.menu} theme="dark">
+        <Menu.Item><NavLink to="/">Home</NavLink></Menu.Item>
+        <Menu.Item><NavLink to="/login">Login</NavLink></Menu.Item>
+        <Menu.Item><NavLink to="/register">Register</NavLink></Menu.Item>
+      </Menu>
+      <Router></Router>
+    </BrowserRouter>
   );
 }
 
